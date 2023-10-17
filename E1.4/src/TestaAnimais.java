f164d02180bd8660871d5cd3026fe23ccb5c7026
Anthony <anthony_felipe@discente.ufg.br>
2023-10-16 22:20:00 -0300

    final da classe TestaAnimais

--- E1.4/src/TestaAnimais.java
+++ E1.4/src/TestaAnimais.java
@@ -1,12 +1,12 @@
 public class TestaAnimais {
     public static void main(String[] args){
-        Animal som[] = new Animal[3];
-        som[0] = new Sapo();
-        som[1] = new Cachorro();
-        som[2] = new Boi();
+        Animal animais[] = new Animal[3];
+        animais[0] = new Sapo();
+        animais[1] = new Cachorro();
+        animais[2] = new Boi();
 
         for(int i = 0; i<3; i++){
-            System.out.println(som[i]);
+            System.out.println(animais[i].som());
         }
 
     }
